cae5cdb26540f7badd9a195c00219dfd8755c27b
Matthew Heun <matthew.heun@me.com>
2020-12-29 11:44:12 -0500

    Wroking on graphing.

--- R/graphs.R
+++ R/graphs.R
@@ -8,12 +8,128 @@
 #' @examples
 #' load_eeu_data() %>% 
 #'   rebound_analysis() %>% 
-#'   cost_graph()
-cost_graph <- function(.rebound_data) {
-  # Make a data frame to be plotted.
-  DF <- .rebound_data %>% 
-    dplyr::select(eeu_base_params$case, eeu_base_params$original, eeu_base_params$upgrade,
-                  starts_with("C_dot"), starts_with("Delta_C"))
+#'   rebound_paths()
+rebound_paths <- function(.rebound_data, 
+                          indexed = FALSE,
+                          I_E = ReboundTools::eeu_base_params$I_E,
+                          E_dot_s_orig = ReboundTools::orig_vars$E_dot_s_orig,
+                          E_dot_emb_orig = ReboundTools::orig_vars$E_dot_emb_orig,
+                          C_dot_md_orig = ReboundTools::orig_vars$C_dot_md_orig,
+                          C_dot_o_orig = ReboundTools::orig_vars$C_dot_o_orig,
+                          S_dot_dev = ReboundTools::star_vars$S_dot_dev, 
+                          C_dot_s_orig = ReboundTools::orig_vars$C_dot_s_orig, 
+                          C_dot_cap_orig = ReboundTools::orig_vars$C_dot_cap_orig, 
+                          G_dot = ReboundTools::star_vars$G_dot,
+                          
+                          S_dot_dev_colour = "red", 
+                          S_dot_dev_linetype = "dashed",
+                          
+                          
+                          energy_type = "Energy",
+                          cost_type = "Cost", 
+                          prefs_type = "Preferences") {
   
+  # The strategy here is to make each segment individually, 
+  # starting from the original point, and using Deltas for everything else.
+ 
+  # A metadata data frame for all these segments
+  meta <- .rebound_data %>% 
+    dplyr::select(eeu_base_params$reference, eeu_base_params$case, 
+                  eeu_base_params$original, eeu_base_params$upgrade)
   
+  # S_dot_dev segment for energy graph
+  x_orig_energy <- .rebound_data[[E_dot_s_orig]]
+  y_orig_energy <- .rebound_data[[E_dot_emb_orig]] + 
+    (.rebound_data[[C_dot_md_orig]] + .rebound_data[[C_dot_o_orig]]) * .rebound_data[[I_E]]
+  xend <- x_orig_energy - .rebound_data[[S_dot_dev]]
+  yend <- y_orig_energy
+  out <- add_segment(indexed = indexed,
+                     colour = S_dot_dev_colour, linetype = S_dot_dev_linetype,
+                     meta = meta, graph_type = energy_type, segment_name = "S_dot_dev", 
+                     x_orig = x_orig_energy, y_orig = y_orig_energy,
+                     x = x_orig_energy, y = y_orig_energy, xend = xend, yend = yend)
+  
+  # G_dot segment for cost graph
+  x_orig_cost <- .rebound_data[[C_dot_s_orig]]
+  y_orig_cost <- .rebound_data[[C_dot_cap_orig]] + .rebound_data[[C_dot_md_orig]] + .rebound_data[[C_dot_o_orig]]
+  xend <- x_orig_cost - .rebound_data[[G_dot]]
+  yend <- y_orig_cost
+  out <- out %>% 
+    add_segment(indexed = indexed,
+                colour = S_dot_dev_colour, linetype = S_dot_dev_linetype,
+                meta = meta, graph_type = cost_type, segment_name = "G_dot", 
+                x_orig = x_orig_cost, y_orig = y_orig_cost,
+                x = x_orig_cost, y = y_orig_cost, xend = xend, yend = yend)
+}
+
+
+#' Add a line segment to a data frame
+#'
+#' @param .DF 
+#' @param indexed 
+#' @param meta 
+#' @param graph_type 
+#' @param segment_name 
+#' @param colour 
+#' @param linetype 
+#' @param x_orig 
+#' @param y_orig 
+#' @param x 
+#' @param y 
+#' @param xend 
+#' @param yend 
+#'
+#' @return A version of `.DF` with line segments added as the final row.
+#' 
+#' @export
+#'
+#' @examples
+add_segment <- function(.DF = NULL, indexed, meta, graph_type, segment_name, 
+                        colour = "black", linetype = "solid",
+                        x_orig, y_orig,
+                        x, y, xend, yend) {
+  if (indexed) {
+    x <- x/x_orig
+    y <- y/y_orig 
+    xend <- xend/x_orig
+    yend <- yend/y_orig
+  }
+  out <- meta %>% 
+    dplyr::mutate(
+      graph_type = graph_type, 
+      segment_name = segment_name,
+      colour = colour, 
+      linetype = linetype,
+      x = x, 
+      y = y, 
+      xend = xend, 
+      yend = yend
+    )
+  if (is.null(.DF)) {
+    return(out)
+  }
+  .DF %>% 
+    dplyr::bind_rows(out)
+}
+
+
+
+
+#' Create a path map for rebound analysis
+#'
+#' @param .path_data The paths to be graphed
+#'
+#' @return A ggplot2 object with the graphs
+#' 
+#' @export
+#'
+#' @examples
+#' load_eeu_data() %>% 
+#'   rebound_analysis() %>% 
+#'   rebound_paths() %>% 
+#'   rebound_graphs()
+rebound_graphs <- function(.path_data) {
+  .path_data %>% 
+    ggplot2::ggplot() +
+    ggplot2::geom_segment(mapping = ggplot2::aes(x = x, y = y, xend = xend, yend = yend))
 }
--- tests/testthat/test-graphs.R
+++ tests/testthat/test-graphs.R
@@ -1,5 +1,62 @@
-test_that("cost_graph() works as expected", {
+test_that("rebound_paths() works as expected", {
+  paths <- load_eeu_data() %>% 
+    rebound_analysis() %>% 
+    rebound_paths()
+  
+  
+  expect_equal(paths)
+  
+  
+})
+
+
+test_that("graphs works as expected", {
   load_eeu_data() %>% 
     rebound_analysis() %>% 
-    cost_graph()
+    rebound_paths() %>% 
+    rebound_graphs() +
+    ggplot2::facet_grid(rows = ggplot2::vars(graph_type), cols = ggplot2::vars(Case))
+})
+
+
+
+test_that("add_segment() works as expected", {
+  meta <- tibble::tibble(Case = "Test case")
+  res <- add_segment(indexed = FALSE, meta = meta, graph_type = "Test type", 
+                     segment_name = "Test segment", 
+                     x_orig = 10, y_orig = 10, 
+                     x = 20, y = 30, xend = 40, yend = 50)
+  expect_equal(res$Case, "Test case")
+  expect_equal(res$graph_type, "Test type")
+  expect_equal(res$segment_name, "Test segment")
+  expect_equal(res$colour, "black")
+  expect_equal(res$linetype, "solid")
+  expect_equal(res$x, 20)
+  expect_equal(res$y, 30)
+  expect_equal(res$xend, 40)
+  expect_equal(res$yend, 50)
+  
+  # Now try with vectors, adding 
+  meta2 <- tibble::tibble(Case = c("Test case 1", "Test case 2"))
+  res2 <- res %>% 
+    add_segment(indexed = FALSE, meta = meta2, graph_type = "Test type 2", 
+                segment_name = "Test segment 2", 
+                x_orig = 10, y_orig = 10, 
+                x = c(20, 30), y = c(30, 40), xend = c(40, 50), yend = c(50, 60))
+  expect_equal(res2$x, c(20, 20, 30))
+  expect_equal(res2$y, c(30, 30, 40))
+  expect_equal(res2$xend, c(40, 40, 50))
+  expect_equal(res2$yend, c(50, 50, 60))
+
+  
+  # Now try normalizing
+  res3 <- res %>% 
+    add_segment(indexed = TRUE, meta = meta2, graph_type = "Test type 3", 
+                segment_name = "Test segment 3", 
+                x_orig = 10, y_orig = 10, 
+                x = c(20, 30), y = c(30, 40), xend = c(40, 50), yend = c(50, 60))
+  expect_equal(res3$x, c(20, 2, 3))
+  expect_equal(res3$y, c(30, 3, 4))
+  expect_equal(res3$xend, c(40, 4, 5))
+  expect_equal(res3$yend, c(50, 5, 6))
 })
