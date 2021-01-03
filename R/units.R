fee8854b4a1de94b4e9ed3bb331ac4700d28b257
Matthew Heun <matthew.heun@me.com>
2021-01-02 21:47:49 -0500

    Working on units tests.

--- R/units.R
+++ R/units.R
@@ -44,33 +44,95 @@ var_units <- function(.rebound_data = load_eeu_data() %>% rebound_analysis(),
 #'
 #' @examples
 units <- function(.var_name, service_unit, energy_engr_unit, 
+                  escape_latex = FALSE,
                   surround_left = "[", 
                   surround_right = "]",
                   leading_delta = "^Delta_", 
                   trailing_stage = "_[^_]*$", 
                   energy_si = "MJ",
+                  currency = "$", 
+                  currency_latex = "\\$",
+                  unitless = "-", 
+                  unitless_latex = "--",
+                  p_E_engr_units = "p_E_engr_units",
+                  p_E = "p_E",
                   efficiency_engy_units = "eta_engr_units",
+                  k = "k", 
+                  I_E = "I_E",
+                  elasticities = "e_",
                   efficiency = "eta", 
                   cost_rate = "C_dot",
-                  currency = "$", 
+                  cost = "C_",
+                  energy_rate = "E_dot", 
+                  energy = "E",
                   time = "year") {
   
-  
+  if (escape_latex) {
+    currency <- currency_latex
+    unitless <- unitless_latex
+  }
   sapply(.var_name, function(var){
-    # Get rid of leading "Delta_"
-    v <- sub(pattern = leading_delta, replacement = "", var)
-    # Get rid of trailing "_orig" and similar.
-    v <- sub(pattern = trailing_stage, replacement = "", v)
-    if (startsWith(v, cost_rate)) {
-      out <- paste0(currency, "/", time)
-    } else if (startsWith(v, efficiency_engy_units)) {
-      out <- paste0(service_unit, "/", energy_engr_unit)
+    
+    # Price of energy
+    
+    if (startsWith(.var_name, p_E_engr_units)) {
+      out <- paste0(currency, "/", energy_engr_unit)
+    } else if (startsWith(.var_name, p_E)) {
+      out <- paste0(currency, "/", energy_si)
+    } 
+    
+    # Energy intensity of the economy
+    
+    else if (startsWith(.var_name, I_E)) {
+      out <- paste0(energy_si, "/", currency)
+    } 
+    
+    # k or elasticities (both are unitless)
+    
+    else if (startsWith(.var_name, k) | startsWith(.var_name, elasticities)) {
+      out <- unitless
     }
     
+    # At this point, assume we have a variable with a leading "Delta_"
+    # or a trailing "_<<stage>>". 
+    
+    else {
+      # Get rid of leading "Delta_"
+      v <- sub(pattern = leading_delta, replacement = "", var)
+      # Get rid of trailing "_orig" and similar.
+      v <- sub(pattern = trailing_stage, replacement = "", v)
+      
+      # Cost rate and cost
+      
+      if (startsWith(v, cost_rate)) {
+        out <- paste0(currency, "/", time)
+      } else if (startsWith(v, cost)) {
+        out <- currency
+      } else if (startsWith(v, efficiency_engy_units)) {
+        out <- paste0(service_unit, "/", energy_engr_unit)
+      }
+      
+      # Energy rate and energy
+      
+      else if (startsWith(v, energy_rate)) {
+        out <- paste0(energy_si, "/", time)
+      } else if (startsWith(v, energy)) {
+        out <- energy_si
+      }
+      
+      # No valid variable found.
+      
+      else {
+        stop(paste("Didn't understand", .var_name, "in units()."))
+      }
+    }
+    
+    # Deal with surrounding characters, if requested
     if (!is.null(surround_left) & !is.null(surround_right)) {
       out <- paste0(surround_left, out, surround_right)
     }
-    out
+    
+    return(out)
   })
 
     
--- tests/testthat/test-units.R
+++ tests/testthat/test-units.R
@@ -7,4 +7,27 @@ test_that("units() works as expected", {
                c(Delta_C_dot_o_hat = "$/year"))
   expect_equal(units("Delta_C_dot_o_hat", service_unit = su, energy_engr_unit = eu, surround_right = NULL), 
                c(Delta_C_dot_o_hat = "$/year"))
+  
+  expect_equal(units("C_dot_s_orig", service_unit = su, energy_engr_unit = eu), 
+               c(C_dot_s_orig = "[$/year]"))
+  expect_equal(units("C_dot_s_orig", service_unit = su, energy_engr_unit = eu, escape_latex = TRUE), 
+               c(C_dot_s_orig = "[\\$/year]"))
+
+  expect_equal(units("C_cap_orig", service_unit = su, energy_engr_unit = eu, escape_latex = TRUE), 
+               c(C_cap_orig = "[\\$]"))
+  
+  expect_equal(units("E_dot_s_orig", service_unit = su, energy_engr_unit = eu, escape_latex = TRUE), 
+               c(E_dot_s_orig = "[MJ/year]"))
+  
+  expect_equal(units("E_emb_orig", service_unit = su, energy_engr_unit = eu, escape_latex = TRUE), 
+               c(E_emb_orig = "[MJ]"))
+  
+  expect_equal(units("p_E_engr_units", service_unit = su, energy_engr_unit = eu, escape_latex = TRUE), 
+               c(p_E_engr_units = "[\\$/energy]"))
+  expect_equal(units("p_E", service_unit = su, energy_engr_unit = eu, escape_latex = TRUE), 
+               c(p_E = "[\\$/energy]"))
+  
+  expect_equal(units("I_E", service_unit = su, energy_engr_unit = eu, escape_latex = TRUE), 
+               c(I_E = "[MJ/\\$]"))
+  
 })
