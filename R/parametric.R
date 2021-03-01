d166e191dba7eea394a86bf42de4455f01146c89
MatthewHeun <matthew.heun@me.com>
2021-02-28 21:51:35 -0500

    Better tests for parametric_studies

--- R/parametric.R
+++ R/parametric.R
@@ -3,7 +3,7 @@
 #' After defining a base case for rebound analyses,
 #' parametric analyses and sensitivity studies are 
 #' often helpful.
-#' This function provides a way to do parametric studies.
+#' This function provides a convenient way to do parametric studies.
 #' 
 #' `original_cases` must have the same structure as the example data frames in this package.
 #' See the file at `sample_eeu_data_path()`.
@@ -27,7 +27,11 @@
 #' @export
 #'
 #' @examples
-#' 
+#' car_case <- load_eeu_data() %>% 
+#'   dplyr::filter(.data[[ReboundTools::eeu_base_params$case]] == "Car")
+#' params <- list(k = seq(0, 2, by = 1), p_E_engr_units = seq(1.5, 2.5, by = 0.25))
+#' res <- parametric_studies(car_case, params)
+#' dplyr::glimpse(res)
 parametric_studies <- function(original_cases, parameterization, 
                                case = ReboundTools::eeu_base_params$case) {
   
@@ -37,21 +41,17 @@ parametric_studies <- function(original_cases, parameterization,
   # Assert all item names in parameterization also appear in original_case
   assertthat::assert_that(all(names(parameterization) %in% names(original_cases)))
   
-  # Make a new data frame of cases
+  # Make a new data frame of parameters to be used in the parametric analysis.
   params <- expand.grid(parameterization)
+
   # Make a data frame with lots of replicated rows, one for each row of params.
   # Code adapted from https://stackoverflow.com/questions/8753531/repeat-rows-of-a-data-frame-n-times
-  cases <- original_cases[rep(seq_len(nrow(original_cases)), nrow(params)), ]
-  # Join params to cases, thereby replacing the same columns in cases.
-  cases <- dplyr::right_join(original_cases, params, by = case)
-  
-  # Analyze the cases
-  
-  
-  
-  # Return the results
-  
-  
-  
-  
+  original_cases[rep(seq_len(nrow(original_cases)), nrow(params)), ] %>% 
+    # Eliminate columns that appear in params.
+    # This step gets rid of columns that we now want to vary below.
+    dplyr::select(-names(params)) %>% 
+    # Bind params to cases to complete the data frame for analysis.
+    dplyr::bind_cols(params) %>% 
+    # Analyze the cases in the parametric study
+    rebound_analysis()
 }
--- tests/testthat/test-parametric.R
+++ tests/testthat/test-parametric.R
@@ -1,7 +1,37 @@
-test_that("parametric uses expand.grid", {
+test_that("parametric_studies() fails when >1 case is sent", {
+  two_cases <- load_eeu_data()
+  params <- list(k = seq(0, 2, by = 0.5))
+  expect_error(parametric_studies(two_cases, params), "nrow\\(original_cases\\) not equal to 1")
+})
+
+test_that("parametric_studies() fails when wrong variables are sent", {
+  lamp_case <- load_eeu_data() %>% 
+    dplyr::filter(.data[[ReboundTools::eeu_base_params$case]] == "Lamp")
+  params <- list(wrong_variable = seq(0, 2, by = 0.5))
+  expect_error(parametric_studies(lamp_case, params),
+               "Elements 1 of names\\(parameterization\\) %in% names\\(original_cases\\) are not true")
+})
+
+test_that("parametric_studies() uses expand.grid", {
   car_case <- load_eeu_data() %>% 
     dplyr::filter(.data[[ReboundTools::eeu_base_params$case]] == "Car")
-  params <- list(k = seq(0.1, 2, by = 0.1), p_E_engr_units = seq(1.5, 2.5, by = 0.1))
+  params <- list(seq(0, 2, by = 0.5), seq(1.5, 2.5, by = 0.25)) %>% 
+    magrittr::set_names(c(ReboundTools::eeu_base_params$k, 
+                          ReboundTools::eeu_base_params$p_E_engr_units))
   res <- parametric_studies(car_case, params)
-  expect_equal(res[["k", "p_E_engr_units"]], expand.grid(params))
+  expect_equal(res %>% 
+                 dplyr::select(c(ReboundTools::eeu_base_params$k,
+                                 ReboundTools::eeu_base_params$p_E_engr_units)) %>% 
+                 as.data.frame(),
+               expand.grid(params), ignore_attr = TRUE)
+  
+  # Check that all rows are same (except for the k and p_E_engr_units columns)
+  res_no_param_vars <- res %>% 
+    dplyr::select(names(car_case)) %>% 
+    dplyr::select(-c(ReboundTools::eeu_base_params$k, ReboundTools::eeu_base_params$p_E_engr_units))
+  expected <- car_case %>% 
+    dplyr::select(-c(ReboundTools::eeu_base_params$k, ReboundTools::eeu_base_params$p_E_engr_units))
+  for (i in 1:nrow(res)) {
+    expect_equal(dplyr::slice(res_no_param_vars, i), expected)
+  }
 })
