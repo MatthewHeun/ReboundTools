75f5ec7ca78f8105b3960529801cbb12202b3397
MatthewHeun <MatthewHeun@users.noreply.github.com>
2021-03-01 19:15:56 -0500

    parametric now up and running.

--- R/parametric.R
+++ R/parametric.R
@@ -9,18 +9,27 @@
 #' See the file at `sample_eeu_data_path()`.
 #' Failures will occur if `original_cases` contains columns calculated by `rebound_analysis()`.
 #' 
-#' `parameterization` must have structure where 
-#' each element is named for a variable in `original_cases`, 
-#' Values of each item in the list should be numerical values to be swept.
+#' `parameterization` is a list and must have the following structure. 
+#' Top-level elements are named for cases.
+#' Case names must match the case column in `original_cases`.
+#' Each top-level element in `parameterization` must itself also be a list.
+#' Each 2nd-level list must contain named vectors, with each name corresponding
+#' to a parameter in `original_cases` that will be swept 
+#' in the parametric study. 
+#' If more than one entry is included in a 2nd-level list, 
+#' all combinations of values will be used, 
+#' via `expand.grid()`..
 #' Note that the original value of each parameter (in `original_cases`)
 #' will not be inserted into the `parameterization` lists,
 #' so be sure to include that value, if desired.
-#' `expand.grid()` is called on the entries of `parameterization`
-#' to cover the union of all parameter values in the parametric study.
+#' 
+#' Note that the case names in `parameterization` (i.e., the names of the top-level entries)
+#' are filtering toward `original_cases`. 
+#' I.e., the output of this function will contain only those cases in `parameterization`.
 #'
 #' @param original_cases A data frame with baseline data from which parametric studies are desired.
 #'                       This data frame is expected to contain 1 row, a single case.
-#' @param parameterization A named list. 
+#' @param parameterization A named list of lists. See details. 
 #' @param case_colname The name of the column of cases. Default is `ReboundTools::eeu_base_params$case`.
 #'
 #' @return A data frame containing results of parametric studies.
@@ -30,69 +39,61 @@
 #' @examples
 #' car_case <- load_eeu_data() %>% 
 #'   dplyr::filter(.data[[ReboundTools::eeu_base_params$case]] == "Car")
-#' params <- list(k = seq(0, 2, by = 1), p_E_engr_units = seq(1.5, 2.5, by = 0.25))
+#' # This example creates a parametric study for the "Car" case only,
+#' # sweeping through values of both `k` and `p_E_engr_units`.
+#' params <- list(Car = list(k = seq(0, 2, by = 1), 
+#'                           p_E_engr_units = seq(1.5, 2.5, by = 0.25)))
 #' res <- parametric_studies(car_case, params)
 #' dplyr::glimpse(res)
 parametric_studies <- function(original_cases, parameterization, 
                                case_colname = ReboundTools::eeu_base_params$case) {
   
-  # # Assert single row in original_cases
-  # assertthat::assert_that(nrow(original_cases) == 1)
-  # 
-  # # Assert all item names in parameterization also appear in original_case
-  # assertthat::assert_that(all(names(parameterization) %in% names(original_cases)))
-  # 
-  # # Make a new data frame of parameters to be used in the parametric analysis.
-  # params <- expand.grid(parameterization)
-  # 
-  # # Make a data frame with lots of replicated rows, one for each row of params.
-  # # Code adapted from https://stackoverflow.com/questions/8753531/repeat-rows-of-a-data-frame-n-times
-  # original_cases[rep(seq_len(nrow(original_cases)), nrow(params)), ] %>% 
-  #   # Eliminate columns that appear in params.
-  #   # This step gets rid of columns that we now want to vary below.
-  #   dplyr::select(-names(params)) %>% 
-  #   # Bind params to cases to complete the data frame for analysis.
-  #   dplyr::bind_cols(params) %>% 
-  #   # Analyze the cases in the parametric study
-  #   rebound_analysis()
-
+  cases <- names(parameterization)
   
-
   # Assert all cases in parameterization are in original_cases
-  assertthat::assert_that(all(names(parameterization) %in% original_cases[[case_colname]]))  
+  assertthat::assert_that(all(cases %in% original_cases[[case_colname]]))
 
-  # Assert all item names in each case's parameterization 
+  # Get a list of unique parameters for each case.
+  params <- lapply(parameterization, FUN = function(case) {
+    names(case)
+  }) %>% 
+    unique()
+  # Make sure all of the parameters for each case are same.
+  # If parameters for all cases are the same, 
+  # the number of unique parameter lists will be 1.
+  assertthat::assert_that(length(params) == 1)
+  # We got a list of length 1, so unlist it
+  # to create a vector of strings to be used later.
+  params <- unlist(params)
+  
+  # Make sure that all params in parameterization
   # also appear as column names in original_cases.
-  lapply(parameterization, FUN = function(case) {
-    assertthat::assert_that(all(names(case) %in% names(original_cases)))
-  })
+  # This check guards against typos.
+  assertthat::assert_that(all(params %in% names(original_cases)))
   
-  # Make sure that varying parameters are same for all cases in parameterization
-  
-  
-  # Set a variable that contains the varying parameters
-  
-  
-  
-  # Make a data frame of cases and parameter variations for each case
-  per_case_parameters <- list()
-  for (i in 1:length(parameterization)) {
-    case_name <- names(parameterization)[[i]]
-    case_params <- parameterization[[i]]
-    per_case_parameters[[i]] <- expand.grid(case_params) %>% 
+  # Build a data frame of all possible combinations of parameter values.
+  param_combinations <- lapply(cases, FUN = function(case_name) {
+    # Get the parameter values for this case
+    parameterization[[case_name]] %>% 
+      # Make a data frame out of all combinations of this case's parameters
+      expand.grid() %>% 
+      # Add the name of the case to the data frame of this case's parameters
       dplyr::mutate(
         "{case_colname}" := case_name
       )
-  }
-  param_cases <- dplyr::bind_rows(per_case_parameters)
+  }) %>% 
+    dplyr::bind_rows()
   
+  # Now do the rebound analysis
   original_cases %>% 
-    # Eliminate the columns that will be parameterized
-    dplyr::select(-names(parameterization)) %>% 
-    dplyr::right_join(param_cases, by = case_colname) %>% 
-    # Analyze the cases in the parametric study
+    # Focus only on the cases of interest
+    dplyr::filter(.data[[case_colname]] %in% cases) %>% 
+    # Eliminate the columns that are to be parameterized, leaving
+    # only unparameterized variables
+    dplyr::select(-dplyr::all_of(params)) %>% 
+    # Join the parameterized variables in param_cases to the unparameterized variables
+    dplyr::right_join(param_combinations, by = case_colname) %>% 
+    # Now we have a data frame that contains all parameters to be swept.
+    # Analyze all cases in the parametric study.
     rebound_analysis()
-  
-  
-  
 }
--- tests/testthat/test-parametric.R
+++ tests/testthat/test-parametric.R
@@ -1,35 +1,33 @@
-test_that("parametric_studies() fails when >1 case is sent", {
-  two_cases <- load_eeu_data()
-  params <- list(k = seq(0, 2, by = 0.5))
-  expect_error(parametric_studies(two_cases, params), "nrow\\(original_cases\\) not equal to 1")
+test_that("parametric_studies() works when 1 case is requested", {
+  one_case <- load_eeu_data() 
+  params <- list(Lamp = list(k = seq(0, 2, by = 0.5)))
+  res <- parametric_studies(one_case, params)
+  expect_equal(unique(res$Case), "Lamp")
 })
 
 test_that("parametric_studies() fails when wrong variables are sent", {
-  lamp_case <- load_eeu_data() %>% 
-    dplyr::filter(.data[[ReboundTools::eeu_base_params$case]] == "Lamp")
-  params <- list(wrong_variable = seq(0, 2, by = 0.5))
+  lamp_case <- load_eeu_data()
+  params <- list(Lamp = list(wrong_variable = seq(0, 2, by = 0.5)))
   expect_error(parametric_studies(lamp_case, params),
-               "Elements 1 of names\\(parameterization\\) %in% names\\(original_cases\\) are not true")
+               "Elements 1 of params %in% names\\(original_cases\\) are not true")
 })
 
 test_that("parametric_studies() uses expand.grid", {
-  car_case <- load_eeu_data() %>% 
-    dplyr::filter(.data[[ReboundTools::eeu_base_params$case]] == "Car")
-  params <- list(seq(0, 2, by = 0.5), seq(1.5, 2.5, by = 0.25)) %>% 
-    magrittr::set_names(c(ReboundTools::eeu_base_params$k, 
-                          ReboundTools::eeu_base_params$p_E_engr_units))
+  car_case <- load_eeu_data() 
+  params <- list(Car = list(k = seq(0, 2, by = 0.5), 
+                            p_E_engr_units = seq(1.5, 2.5, by = 0.25)))
   res <- parametric_studies(car_case, params)
   expect_equal(res %>% 
-                 dplyr::select(c(ReboundTools::eeu_base_params$k,
-                                 ReboundTools::eeu_base_params$p_E_engr_units)) %>% 
+                 dplyr::select(c("k", "p_E_engr_units")) %>% 
                  as.data.frame(),
-               expand.grid(params), ignore_attr = TRUE)
+               expand.grid(params$Car), ignore_attr = TRUE)
   
   # Check that all rows are same (except for the k and p_E_engr_units columns)
   res_no_param_vars <- res %>% 
     dplyr::select(names(car_case)) %>% 
     dplyr::select(-c(ReboundTools::eeu_base_params$k, ReboundTools::eeu_base_params$p_E_engr_units))
   expected <- car_case %>% 
+    dplyr::filter(Case == "Car") %>% 
     dplyr::select(-c(ReboundTools::eeu_base_params$k, ReboundTools::eeu_base_params$p_E_engr_units))
   for (i in 1:nrow(res)) {
     expect_equal(dplyr::slice(res_no_param_vars, i), expected)
@@ -39,7 +37,27 @@ test_that("parametric_studies() uses expand.grid", {
 
 test_that("parametric_studies() works for more than 1 case", {
   cases <- load_eeu_data()
-  params <- list(Car = list(k = seq(1, 2, by = 0.5), p_E_engr_units = seq(1.5, 2.5, by = 0.25)), 
-                 Lamp = list(k = seq(0.5, 1.5, by = 0.5), p_E_engr_units = seq(0.05, 0.15, by = 0.05)))
-  res <- parametric_studies(cases, params)
+  # Make parameters that don't vary
+  params_1 <- list(Car = list(k = rep(1, times = 10), p_E_engr_units = rep(1.5, times = 2)), 
+                   Lamp = list(k = rep(1.5, times = 3), p_E_engr_units = rep(0.05, times = 5)))
+  res_1 <- parametric_studies(cases, params_1)
+  # Every row in res should be the same (per case)
+  for (case in unique(res_1$Case)) {
+    res_per_case <- res_1 %>% dplyr::filter(.data[["Case"]] == case)
+    unique_rows <- unique(res_per_case)
+    expect_equal(nrow(unique_rows), 1)
+  }
+  
+  # Now test with different parameters on each row
+  params_2 <- list(Car = list(k = seq(1, 2, by = 0.5), p_E_engr_units = seq(1.5, 2.5, by = 0.25)), 
+                   Lamp = list(k = seq(0.5, 1.5, by = 0.5), p_E_engr_units = seq(0.05, 0.15, by = 0.05)))
+  res_2 <- parametric_studies(cases, params_2)
+
+  # Check that we have something.  
+  expect_true(!is.null(res_2))
+  # Check that we have both cases in the result
+  expect_equal(unique(res_2$Case), c("Car", "Lamp"))
+  # Ensure that each row is different
+  unique_rows_2 <- unique(res_2)
+  expect_equal(nrow(unique_rows_2), nrow(res_2))
 })
