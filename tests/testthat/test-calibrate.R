70a663650911cdf4fcf8e8f7fea7d6e172613bd0
MatthewHeun <MatthewHeun@users.noreply.github.com>
2021-04-21 19:38:58 -0400

    Improved test.

--- tests/testthat/test-calibrate.R
+++ tests/testthat/test-calibrate.R
@@ -6,4 +6,10 @@ test_that("calibrate_k() works as expected", {
   expect_equal(new_k[[2]], 1.7783998)
   expect_equal(names(new_k)[[1]], "Car")
   expect_equal(names(new_k)[[2]], "Lamp")
+  
+  # Add calibration with 2 Re_tot_target values.
+  new_k_2 <- load_eeu_data() %>% 
+    rebound_analysis() %>% 
+    calibrate_k(c(0.4, 0.7))
+  expect_equal(new_k_2[[1]], )
 })
