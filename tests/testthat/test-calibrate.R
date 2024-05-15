9112217ce982bb52f43d201eaa8a9df3fa9bd2a0
Matthew Heun <matthew.heun@me.com>
2024-05-15 09:52:07 -0400

    Test coverage back to 100%

--- tests/testthat/test-calibrate.R
+++ tests/testthat/test-calibrate.R
@@ -4,13 +4,16 @@ test_that("calibrate_k() works as expected", {
     calibrate_k(0.63)
   expect_equal(new_k[[1]], 6.2555914)
   expect_equal(new_k[[2]], 2.3800704986831138)
+  expect_equal(new_k[[3]], 6.376461)
   expect_equal(names(new_k)[[1]], "Car")
   expect_equal(names(new_k)[[2]], "Lamp")
+  expect_equal(names(new_k)[[3]], "Car, r = 0")
   
   # Add calibration with 2 Re_tot_target values.
   new_k_2 <- load_eeu_data() %>% 
     rebound_analysis() %>% 
-    calibrate_k(c(0.4, 0.7))
+    calibrate_k(c(0.4, 0.7, 1.0))
   expect_equal(new_k_2[[1]], 3.2246189747379455)
   expect_equal(new_k_2[[2]], 2.8718661404585002)
+  expect_equal(new_k_2[[3]], 11.327695)
 })
